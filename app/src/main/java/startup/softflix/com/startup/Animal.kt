ad52b590fb00b13761b0dfa0ee3155c15ca2c6a0
Zanis Ali <zanisali999@gmail.com>
2018-12-13 21:11:47 +0100

    initial commit

---
+++ app/src/main/java/startup/softflix/com/startup/Animal.kt
@@ -0,0 +1,18 @@
+package startup.softflix.com.startup
+
+/**
+ * Created by Zanis on 12/13/2018.
+ */
+
+class Animal{
+    var name:String?=null
+    var des:String?=null
+    var image:Int?=null
+
+    constructor(name:String, des: String, image:Int)
+    {
+        this.name=name
+        this.des=des
+        this.image=image
+    }
+}
